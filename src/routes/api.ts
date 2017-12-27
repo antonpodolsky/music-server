43687f1c8eb851c6d3300163ca4911f6c982c65d
Anton Podolsky <a.thesilentman@gmail.com>
2017-12-27 23:12:35 +0200

    ongoing

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -18,37 +18,54 @@ function fetch(what: 'Children' | 'Files', id) {
   }, (error, response, body) => !error && response.statusCode === 200 ? resolve(body) : reject(error)));
 }
 
+function getArtists(): Promise<any[]> {
+  return fetch('Children', 1000).then(body => {
+    const artists = parser.xml2json(body);
+  
+    return artists.Response.Item.map(artist => ({
+      id: artist['_@ttribute'],
+      name: artist.Name,
+      image: image(artist['_@ttribute'])
+    }));
+  });
+}
+
+function getArtistAlbums(id) {
+  return fetch('Children', id).then(body => {
+    const artist = parser.xml2json(body);
+  
+    const albums = (artist.Response.Item.length ? artist.Response.Item : [artist.Response.Item]).map(album => ({
+      id: album['_@ttribute'],
+      name: album.Name, 
+      image: image(album['_@ttribute'])
+    }));
+  
+    return albums;
+  });
+}
+
+function geAlbumTracks(id) {
+  return fetch('Files', id).then(body => {
+    const tracks = body.split(';').slice(3).map(track => ({
+      id: track, name: track
+    }));
+  
+    return ({tracks});
+  });
+}
+
+function getArtist(id) {
+  return Promise.all([
+    getArtists().then(artists => artists.find(artist => artist.id === id)),
+    getArtistAlbums(id)
+  ]);
+}
+
 function send(res, data) {
   res.set('Content-Type', 'application/json');
   res.send(data);
 }
 
-router.get('/artists', (req, res) => fetch('Children', 1000).then(body => {
-  const artists = parser.xml2json(body);
-
-  return artists.Response.Item.map(artist => ({
-    id: artist['_@ttribute'],
-    name: artist.Name,
-    image: image(artist['_@ttribute'])
-  }));
-}).then(data => send(res, data)));
-
-router.get('/artist/:id', (req, res) => fetch('Children', req.params.id).then(body => {
-  const artist = parser.xml2json(body);
-
-  const albums = (artist.Response.Item.length ? artist.Response.Item : [artist.Response.Item]).map(album => ({
-    id: album['_@ttribute'],
-    name: album.Name, 
-    image: image(album['_@ttribute'])
-  }));
-
-  return {albums};
-}).then(data => send(res, data)));
-
-router.get('/album/:id', (req, res) => fetch('Files', req.params.id).then(body => {
-  const tracks = body.split(';').slice(3).map(track => ({
-    id: track, name: track
-  }));
-
-  return ({tracks});
-}).then(data => send(res, data)));
+router.get('/artists', (req, res) => getArtists().then(data => send(res, data)));
+router.get('/artist/:id', (req, res) => getArtist(req.params.id).then(([artist, albums]) => send(res, {...artist, albums})));
+router.get('/album/:id', (req, res) => geAlbumTracks(req.params.id).then(data => send(res, data)));
